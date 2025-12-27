63336029ab5ac652115914c829382b6947fb2529
saori <matsu9789@gmail.com>
2025-12-27 16:05:19 +0900

    共通部分のlayout化

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashionably Late</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="header__inner">
+            <a href="">
+                Fashionably Late
+            </a>
+        </h1>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
