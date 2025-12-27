f0db4b47d4bfc19a5d5f4865f12b26f0fcd4fce9
saori <matsu9789@gmail.com>
2025-12-27 18:25:32 +0900

    サンクスページHTML/CSS作成

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fashionably Late</title>
     <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
 </head>
 
 <body>
     <main>
-        <p class="message">お問い合わせありがとうございました</p>
-        <button class="button">HOME</button>
-        <div class="background">thank you</div>
+        <div class="message">
+            <p class="message__sentence">お問い合わせありがとうございました</p>
+            <a class="message__link" href="/">HOME</a>
+        </div>
+        <div class="background">Thank you</div>
     </main>
 </body>
 
