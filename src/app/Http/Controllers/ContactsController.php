fc22c515f9ff57056b10bab50cd77e1bd2188fbc
saori <matsu9789@gmail.com>
2025-12-27 22:37:04 +0900

    データベースに保存

--- src/app/Http/Controllers/ContactsController.php
+++ src/app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactsController extends Controller
 {
@@ -20,6 +21,7 @@ class ContactsController extends Controller
     public function store(Request $request)
     {
         $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel1', 'tel2', 'tel3', 'address', 'building', 'content', 'detail']);
-        return view('thanks', compact('contact'));
+        Contact::create($contact);
+        return view('thanks');
     }
 }
