c5a58a9d44672cd5bf0f7f6ffb642eb64c935ce9
saori <matsu9789@gmail.com>
2025-12-31 16:48:38 +0900

    検索画面CSS作成

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fashionably Late</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}" />
 </head>
 
@@ -17,51 +16,60 @@
                 Fashionably Late
             </a>
         </h1>
-        <div>logout</div>
+        <button class="header__logout">logout</button>
     </header>
     <main>
-        <div>
-            <h2>Admin</h2>
-        </div>
-        <form action="/search" method="get">
-            <input type="text" name="input" value="{{request('input')}}" placeholder="名前やメールアドレスを入力してください">
-            <select name="gender" id="">
-                <option value="">性別</option>
-                <option value="1">男性</option>
-                <option value="2">女性</option>
-                <option value="3">その他</option>
-            </select>
-            <select name="detail" id="">
-                <option value="">お問い合わせの種類</option>
-                @foreach($categories as $category)
-                <option value="{{ $category['id'] }}">{{ $category['content'] }}</option>
+        <div class="admin__content">
+            <div class="admin__heading">
+                <h2>Admin</h2>
+            </div>
+            <form action="/search" method="get">
+                <div class="admin__input">
+                    <div class="admin__input-text">
+                        <input type="text" name="input" value="{{request('input')}}" placeholder="名前やメールアドレスを入力してください">
+                    </div>
+                    <select class="admin__input-select" name="gender" id="">
+                        <option value="">性別</option>
+                        <option value="1">男性</option>
+                        <option value="2">女性</option>
+                        <option value="3">その他</option>
+                    </select>
+                    <select class="admin__input-select" name="detail" id="">
+                        <option value="">お問い合わせの種類</option>
+                        @foreach($categories as $category)
+                        <option value="{{ $category['id'] }}">{{ $category['content'] }}</option>
+                        @endforeach
+                    </select>
+                    <select class="admin__input-select" name="" id="">
+                        <option value="">年/月/日</option>
+                    </select>
+                    <button class="admin__input-search">検索</button>
+                    <button class="admin__input-reset">リセット</button>
+                </div>
+            </form>
+            <button class="admin__export">エクスポート</button>
+            ページ
+            <table class="table">
+                <tr class="table__header">
+                    <th>お名前</th>
+                    <th>性別</th>
+                    <th>メールアドレス</th>
+                    <th>お問い合わせの種類</th>
+                    <th></th>
+                </tr>
+                @foreach($contacts as $contact)
+                <tr class="table__description">
+                    <td>{{$contact->name}}</td>
+                    <td>{{$contact->gender}}</td>
+                    <td>{{$contact->email}}</td>
+                    <td>{{$contact->detail}}</td>
+                    <td>
+                        <button class="table__description-detail">詳細</button>
+                    </td>
+                </tr>
                 @endforeach
-            </select>
-            <select name="" id="">
-                <option value="">年/月/日</option>
-            </select>
-            <button>検索</button>
-        </form>
-        <button>リセット</button>
-        <button>エクスポート</button>
-        ページ
-        <table>
-            <tr>
-                <th>お名前</th>
-                <th>性別</th>
-                <th>メールアドレス</th>
-                <th>お問い合わせの種類</th>
-            </tr>
-            @foreach($contacts as $contact)
-            <tr>
-                <td>{{$contact->name}}</td>
-                <td>{{$contact->gender}}</td>
-                <td>{{$contact->email}}</td>
-                <td>{{$contact->detail}}</td>
-                <td>詳細</td>
-            </tr>
-            @endforeach
-        </table>
+            </table>
+        </div>
     </main>
 </body>
 
