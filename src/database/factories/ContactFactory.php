0a9629cdb1a337cf8a4d2ce8441b0a01799f07cc
saori <matsu9789@gmail.com>
2026-01-02 21:48:27 +0900

    ファクトリ

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $faker = \Faker\Factory::create('ja_JP');
+
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->secondaryAddress(),
+            'detail' => $this->faker->realText(100)
+        ];
+    }
+}
